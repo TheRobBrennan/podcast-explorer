caacd27367b6f25f713f76717c603895a24414b1
Rob Brennan <rob@therobbrennan.com>
2021-02-06 14:09:33 -0800

    Configure purge option to tree shake unused styles for all components and pages

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  // Configure the purge option with the paths to all of your pages and components so Tailwind can tree-shake unused styles in production builds
+  // https://tailwindcss.com/docs/guides/nextjs#configure-tailwind-to-remove-unused-styles-in-production
+  purge: [
+    "./apollo/**/*.{js,ts,jsx,tsx}",
+    "./heimdall/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
