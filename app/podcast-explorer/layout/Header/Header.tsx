8a401c376f16e969058c0e0492ebceea431a8840
Rob Brennan <rob@therobbrennan.com>
2021-02-06 14:59:10 -0800

    Use fillRule instead of fill-rule within the svg path

--- app/podcast-explorer/layout/Header/Header.tsx
+++ app/podcast-explorer/layout/Header/Header.tsx
@@ -86,7 +86,7 @@ const Header: FC = () => {
                   >
                     <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
                       ></path>
                     </svg>
