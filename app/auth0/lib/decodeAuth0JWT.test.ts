8bc429862a15aeaff8343d69506da9e770f79825
Rob Brennan <rob@therobbrennan.com>
2021-02-06 15:30:59 -0800

    Enable test suite even when Auth0 support is not implemented

--- app/auth0/lib/decodeAuth0JWT.test.ts
+++ app/auth0/lib/decodeAuth0JWT.test.ts
@@ -3,15 +3,17 @@ import { cleanup } from "@testing-library/react"
 import { VALID_JWT_TOKEN, VALID_BUT_EXPIRED_JWT_TOKEN } from "./index"
 import { decodeAuth0JWT as func } from "./decodeAuth0JWT"
 
-describe.skip("Our Auth0 decodeAuth0JWT lib", () => {
+describe("Our Auth0 decodeAuth0JWT lib", () => {
   afterEach(cleanup)
-  describe("when supplied with a valid JWT token", () => {
+
+  // REVISIT: Enable skipped tests if Auth0 support has been added
+  describe.skip("when supplied with a valid JWT token", () => {
     it("should receive a decoded JWT object", async () => {
       const result = await func(VALID_JWT_TOKEN)
       expect(result).toMatchSnapshot()
     })
   })
-  describe("when supplied with a well-formed JWT token that has expired or cannot be verified", () => {
+  describe.skip("when supplied with a well-formed JWT token that has expired or cannot be verified", () => {
     it("should result in an error during processing", async (done) => {
       const result = await func(VALID_BUT_EXPIRED_JWT_TOKEN).catch((e) => {
         if (e.message === "jwt expired") {
@@ -20,6 +22,7 @@ describe.skip("Our Auth0 decodeAuth0JWT lib", () => {
       })
     })
   })
+
   describe("when supplied with an unexpected string", () => {
     it("should result in an error during processing", async (done) => {
       const mockPoorlyFormedAndInvalidJWTToken = `Here is an example string that is neither a JWT token nor a valid string in the HTTP Authorization header`
